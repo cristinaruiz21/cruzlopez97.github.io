2721cb05d0e201af4ae4c901886de49ae1e9ab1e
Cristina Ruiz <cristinaruiz96@yahoo.com>
2021-02-04 20:45:26 -0600

    Updated about me message and contact links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,19 +12,19 @@ function Contact() {
                 <Col size="md-12">
                     <div className="icon LinkedIn">
                         <h3 className="contact">LinkedIn</h3>
-                        <a href="https://www.linkedin.com/in/cristina-ruiz-47527a134" rel="noreferrer" target="_blank" className="fab fa-linkedin"></a>
+                        <a href="https://www.linkedin.com/in/cruz-lopez-calr512" rel="noreferrer" target="_blank" className="fab fa-linkedin"></a>
                     </div>
                     <div className="icon github">
                         <h3 className="contact">GitHub</h3>
-                        <a href="https://github.com/cristinaruiz21" rel="noreferrer" target="_blank" className="fab fa-github"></a>
+                        <a href="https://github.com/Primes512" rel="noreferrer" target="_blank" className="fab fa-github"></a>
                     </div>
                     <div className="icon phone">
-                        <h3 className="contact">(512)-826-7879</h3>
+                        <h3 className="contact">(512)-516-1612</h3>
                         <i className="fas fa-phone"></i>
                     </div>
                     <div className="icon Resume">
                         <h3 className="contact">Resume</h3>
-                        <a href="https://drive.google.com/file/d/17-1yV4E4SKzbmXqKz2vQNW7F29QMNOhw/view?usp=sharing" rel="noreferrer" target="_blank" className="fas fa-file"></a>
+                        <a href="https://drive.google.com/file/d/1EjRoGFDNbOV0PxFfO6sXpGpMyJnmnYCZ/view?usp=sharing" rel="noreferrer" target="_blank" className="fas fa-file"></a>
                     </div>
                 </Col>
             </Row>
